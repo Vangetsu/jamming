359500c8979c97a2748e1ecbc2da99eebc714090
timkuc <timucinn@gmail.com>
2021-04-09 22:47:30 +0300

    add new files

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TrackList from '';
+
+class PlayList extends React.Component {
+    render() {
+        return (
+            <div class="Playlist">
+                <input defaultValue={'New Playlist'}/>
+                <TrackList/>
+                <button class="Playlist-save">SAVE TO SPOTIFY</button>
+            </div>
+        );
+    }
+}
+
+export default PlayList;
+
+
+
+
+
+
+
