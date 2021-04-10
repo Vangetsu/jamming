1e330e47c329a982826c24c7c750f8e1dc7bb825
timkuc <timucinn@gmail.com>
2021-04-10 15:36:14 +0300

    fix components

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Playlist.css';
 import TrackList from '../TrackList/TrackList';
 
-class PlayList extends React.Component {
+class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
@@ -14,7 +14,7 @@ class PlayList extends React.Component {
     }
 }
 
-export default PlayList;
+export default Playlist;
 
 
 
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Track.css';
 
 
 class Track extends React.Component {
 
-
+    renderAction() {
+        if(this.props.isRemoval) {
+            return <button className="Track-action">-</button>
+        } else {
+            return <button className="Track-action">+</button>
+        }
+    }
     /*
     Create a method called renderAction that displays a <button> element with - as its content if the isRemoval property is true, and a + <button> element if the isRemoval property is false. Set the class name to Track-action.
     */
@@ -16,7 +23,7 @@ class Track extends React.Component {
                     {/* <h3><!-- track name will go here --></h3> */}
                     {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
                 </div>
-                {/* <button className="Track-action"><!-- + or - will go here --></button> */}
+                {this.renderAction()}
             </div>
         );
     }
